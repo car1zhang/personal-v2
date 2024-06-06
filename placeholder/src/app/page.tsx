6cf639dde9843b71551aa5471c219e6a95b8759a
Carl Zhang <zhang.carl.x@gmail.com>
2024-06-06 18:11:27 -0400

    haha

--- placeholder/src/app/page.tsx
+++ placeholder/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-4xl font-medium">CARL ZHANG</h1>
       <div className="flex flex-col gap-4 tracking-wide leading-relaxed font-light">
         <p>hi. my name is carl. </p>
-        <p>i solve problems and craft tools. i&apos;m currently building product at goodie labs. i&apos;ll (likely) be studying cs at uwaterloo this fall. </p>
+        <p>i solve problems and craft tools. i&apos;m currently building product at <a href="https://www.usebutler.app/" target="_blank" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">goodie labs</a> and i&apos;ll (likely) be studying <a href="https://cs.uwaterloo.ca/" target="_blank" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">cs at uwaterloo</a> this fall. </p>
         <p>this site is an active wip. in the meantime, you can reach me at <a href="mailto:zhang.carl.x@gmail.com" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">zhang.carl.x@gmail.com</a> or via the platforms below.</p>
       </div>
       <div className="flex gap-8">
