01a935c17071c80fe2527d774b63c84e74a3ddde
Carl Zhang <zhang.carl.x@gmail.com>
2024-06-06 16:08:50 -0400

    text update

--- placeholder/src/app/page.tsx
+++ placeholder/src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <h1 className="text-4xl font-medium">CARL ZHANG</h1>
       <div className="flex flex-col gap-4 tracking-wide leading-relaxed font-light">
         <p>hi. my name is carl. </p>
-        <p>i solve problems and build tools. i&apos;m also going to be studying cs at uwaterloo this fall. </p>
-        <p>this site is currently under construction. in the meantime, you can reach me at <a href="mailto:zhang.carl.x@gmail.com" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">zhang.carl.x@gmail.com</a>.</p>
+        <p>i solve problems and craft tools. i&apos;m currently building product at goodie labs. i&apos;ll (likely) be studying cs at uwaterloo this fall. </p>
+        <p>this site is an active wip. in the meantime, you can reach me at <a href="mailto:zhang.carl.x@gmail.com" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">zhang.carl.x@gmail.com</a> or via the platforms below.</p>
       </div>
       <div className="flex gap-8">
         <a href="https://www.linkedin.com/in/car1zhang/" target="_blank" className="hover:text-light transition">
