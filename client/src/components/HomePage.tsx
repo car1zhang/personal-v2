e6f7b52029771b2422c2310f451a58610a034307
Carl Zhang <zhang.carl.x@gmail.com>
2024-09-01 18:51:48 -0400

    upd

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -6,8 +6,8 @@ export default function HomePage({ className }: { className: string }) {
     <div className={clsx("flex flex-col gap-8", className)}>
       <div className="flex flex-col gap-4 tracking-wide leading-relaxed font-light">
         <p>Hi. My name is Carl. </p>
-        <p>I solve problems and craft tools. I&apos;m currently taking a year off from studying <a href="https://cs.uwaterloo.ca/" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">CS at UWaterloo</a> to build <a href="https://www.usebutler.app/" target="_blank" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">Butler</a>.</p>
-        <p>This site is an active WIP. You can reach me at <a href="mailto:carl@usebutler.app" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">carl@usebutler.app</a>.</p>
+        <p>I solve problems and craft tools. I&apos;m currently taking a year off from studying <a href="https://cs.uwaterloo.ca/" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">CS at UWaterloo</a> to build <a href="https://www.butler.ai/" target="_blank" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">Butler</a>.</p>
+        <p>This site is an active WIP. You can reach me at <a href="mailto:carl@butler.ai" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">carl@butler.ai</a>.</p>
       </div>
       <div className="flex gap-8">
         <a href="https://www.linkedin.com/in/car1zhang/" target="_blank" className="hover:text-light transition">
