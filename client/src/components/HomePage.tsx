9cf09ff504a83f7f2c9f88cc6e1a68f17eedbe08
Carl Zhang <zhang.carl.x@gmail.com>
2024-10-13 14:52:15 -0400

    Update HomePage.tsx

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -6,7 +6,7 @@ export default function HomePage({ className }: { className: string }) {
     <div className={clsx("flex flex-col gap-8", className)}>
       <div className="flex flex-col gap-4 tracking-wide leading-relaxed font-light">
         <p>Hi, my name is Carl. </p>
-        <p>I solve problems and craft tools. I&apos;m currently taking a year off from studying <a href="https://cs.uwaterloo.ca/" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">CS at UWaterloo</a> to build <a href="https://www.butler.ai/" target="_blank" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">Butler</a>.</p>
+        <p>I solve problems. I&apos;m currently taking a year off from studying <a href="https://cs.uwaterloo.ca/" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">CS at UWaterloo</a> to build <a href="https://www.butler.ai/" target="_blank" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">Butler</a>.</p>
         <p>This site is an active WIP. You can reach me at <a href="mailto:carl@butler.ai" className="underline underline-offset-2 decoration-highlight decoration-2 hover:text-light hover:decoration-dark transition">carl@butler.ai</a>.</p>
       </div>
       <div className="flex gap-8">
